899ceef5431e43c0dfa7ff666bdeb1cbc2367ce9
echo <309876101@qq.com>
2019-07-03 18:03:44 +0800

    音乐列表v1

---
+++ pages/list/list.js
@@ -0,0 +1,123 @@
+let res = {
+  "ok": true,
+  "message": "\u63d0\u53d6\u6570\u636e",
+  "data": [
+    [
+      0,
+      "\u4e00\u4e07\u6b21\u60b2\u4f24",
+      "\u9003\u8dd1\u8ba1\u5212",
+      "\u4e16\u754c",
+      4,
+      15
+    ],
+    [
+      1,
+      "\u4e00\u4e07\u6b21\u60b2\u4f24\uff08Cover\uff1a\u9003\u8dd1\u8ba1\u5212\uff09",
+      "\u5218\u81f3\u4f73",
+      "\u4e00\u4e07\u6b21\u60b2\u4f24",
+      0,
+      54
+    ],
+    [
+      2,
+      "\u4e00\u4e07\u6b21\u60b2\u4f24 (Live\u7248)",
+      "\u9003\u8dd1\u8ba1\u5212",
+      "\u58f0\u97f3\u73a9\u5177\uff06\u9003\u8dd1\u8ba1\u5212\u00b7\u4e13\u573a",
+      4,
+      20
+    ],
+    [
+      3,
+      "\u4e00\u4e07\u6b21\u60b2\u4f24 (Acoustic Cover \u9003\u8dd1\u8ba1\u5212)",
+      "CANNIE(\u6674\u5b50)",
+      "\u4e00\u4e07\u6b21\u60b2\u4f24 (Acoustic Cover \u9003\u8dd1\u8ba1\u5212)",
+      3,
+      4
+    ],
+    [
+      4,
+      "\u4e00\u4e07\u6b21\u60b2\u4f24\uff08Cover\uff1a\u5f90\u6b4c\u9633\uff09",
+      "\u5c28\u67a4",
+      "\u8361",
+      4,
+      17
+    ],
+    [
+      5,
+      "\u4e00\u4e07\u6b21\u60b2\u4f24\uff08Cover \u9003\u8dd1\u8ba1\u5212\uff09",
+      "\u5c0f\u4e49\u5b66\u957f",
+      "\u7ffb\u5531\u96c6",
+      4,
+      12
+    ],
+    [
+      6,
+      "\u4e00\u4e07\u6b21\u60b2\u4f24\uff08Cover\uff1a\u5f90\u6b4c\u9633\uff09",
+      "\u9648\u51ac\u9716",
+      "\u4e00\u4e07\u6b21\u60b2\u4f24",
+      4,
+      48
+    ],
+    [
+      7,
+      "\u4e00\u4e07\u6b21\u60b2\u4f24\uff08Cover \u9003\u8dd1\u8ba1\u5212\uff09",
+      "\u5f20\u6f84\u9896",
+      "\u5c0f\u6a59\u5b50\u4e00\u9897",
+      4,
+      21
+    ],
+    [
+      8,
+      "\u4e00\u4e07\u6b21\u60b2\u4f24\uff08\u5409\u4ed6\u7248\uff09\uff08Cover\uff1a\u9003\u8dd1\u8ba1\u5212\uff09",
+      "\u8bb8\u660e\u8fdc",
+      "\u7434\u612b",
+      3,
+      42
+    ],
+    [
+      9,
+      "\u4e00\u4e07\u6b21\u60b2\u4f24\uff08Cover\uff1a\u9003\u8dd1\u8ba1\u5212\uff09",
+      "\u5f20\u7c73\u8fb0",
+      "\u4e00\u4e07\u6b21\u60b2\u4f24",
+      4,
+      21
+    ],
+    [
+      10,
+      "\u300a\u4e00\u4e07\u6b21\u60b2\u4f24\u300b\uff08Cover\uff1a\u9003\u8dd1\u8ba1\u5212\uff09",
+      "\u6b4c\u624bAda",
+      "\u65b0\u7684ADA",
+      1,
+      19
+    ]
+  ]
+}
+// if (res.ok) {
+//   for (let i = 0; i < res.data.length; i++) {
+//     const e = res.data[i]
+//     const name = e[1]
+//     const singer = e[2]
+//   console.log('🐸', name, singer)    
+//   }
+// }
+if (res.ok) {
+  let html = ''
+  for (let i = 0; i < res.data.length; i++) {
+    const e = res.data[i]
+    const name = e[1]
+    const singer = e[2]
+    const album = e[3]
+
+    const min = e[4]
+    const second = e[5]
+    const time = e[4] + ":" + e[5]
+    console.log('🐸', name, singer, album, time)
+    html += `<div class="item">
+  <div class ="name"> ${name}</div>
+  <div class ="singer">${singer}</div>
+  <div class ="album">${album}</div>
+  <div class ="time">${time}</div>
+  </div>`
+  }
+  document.querySelector('#list').innerHTML = html
+}
